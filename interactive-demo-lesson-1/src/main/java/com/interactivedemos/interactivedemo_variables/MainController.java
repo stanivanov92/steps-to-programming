79774eac14ca07b15ef8fac7932a363229fb43cf
Stanislav <stan_ivanov@icloud.com>
2023-01-22 18:48:56 +0000

    Introduction of dynamic generated table columns

--- interactive-demo-lesson-1/src/main/java/com/interactivedemos/interactivedemo_variables/MainController.java
+++ interactive-demo-lesson-1/src/main/java/com/interactivedemos/interactivedemo_variables/MainController.java
@@ -1,49 +1,60 @@
 package com.interactivedemos.interactivedemo_variables;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.SetChangeListener;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     @FXML
-    private TextField decimalInput;
+    private ComboBox<String> comboBox;
+
 
     @FXML
-    private Button convertButton;
+    private TableView deciByteTable;
 
-    @FXML
-    private TableView<TableEntry> deciByteTable;
 
-    @FXML
-    protected void onConvertButtonClick() {
-        var userInput = Integer.parseInt(decimalInput.getText());
-        this.deciByteTable.getItems().add(new TableEntry(userInput, (byte)userInput));
-    }
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Rudimentary input validation.
-        decimalInput.textProperty().addListener((observableValue, oldInput, input) -> {
-            if (input.isBlank()) {
-                convertButton.setDisable(true);
-                return;
-            }
-
-            try {
-                convertButton.setDisable(false);
-                var parsedInput = Integer.parseInt(input);
-
-                if (parsedInput > 127 || parsedInput < -127) {
-                    convertButton.setDisable(true);
-                }
-            } catch (NumberFormatException nfe) {
-                convertButton.setDisable(true);
-            }
+        comboBox.getItems().addAll("byte","short","int","long","double","float", "char","boolean");
+        comboBox.setValue("byte");
+        comboBox.setOnAction(actionEvent -> {
         });
+        setInitialTableSize(8);
+
+    }
+
+    private void setInitialTableSize(int columns){
+        for(int i = 0; i < columns; i++){
+            final int k = i;
+            TableColumn<ObservableList<String>,String> tableColumn = new TableColumn(String.valueOf(i));
+            tableColumn.setCellValueFactory(
+                    (Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>)
+                            param -> new SimpleStringProperty(param.getValue().get(k).toString()));
+            deciByteTable.getColumns().add(tableColumn);
+        }
+        addRow("00000000");
+
+    }
+
+
+    private void addRow(String values ){
+        ObservableList<String> row = FXCollections.observableArrayList(values);
+        deciByteTable.getItems().add(row);
     }
 }
